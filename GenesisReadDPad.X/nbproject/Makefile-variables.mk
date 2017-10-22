7bf66114684f8e057ea7a1568e123027bfcfef00
danielpapajohn <dpapajohn@hotmail.com>
2017-10-22 01:23:27 -0700

    Changed Genesis demo configurations do the project can actually program a PIC24.

---
+++ GenesisReadDPad.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=GenesisReadDPad.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/GenesisReadDPad.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=genesisreaddpad.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/genesisreaddpad.x.tar
